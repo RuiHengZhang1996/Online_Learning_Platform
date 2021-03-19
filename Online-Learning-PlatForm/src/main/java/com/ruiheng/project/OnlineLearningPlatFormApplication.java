12169d2ab868c9c17d640132b488e58d1d30c023
RuiHengZhang1996 <zhangruiheng1996@gmail.com>
2021-03-19 15:22:05 -0400

    BasicConfig

--- Online-Learning-PlatForm/src/main/java/com/ruiheng/project/OnlineLearningPlatFormApplication.java
+++ Online-Learning-PlatForm/src/main/java/com/ruiheng/project/OnlineLearningPlatFormApplication.java
@@ -3,7 +3,7 @@ package com.ruiheng.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class OnlineLearningPlatFormApplication {
 
 	public static void main(String[] args) {
